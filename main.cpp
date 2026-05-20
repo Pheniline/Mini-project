8d3fc780ac0f237e3cada2b17180cb804ce35652
Pheniline <phennyjer@gmail.com>
2026-05-20 18:43:26 +0300

    GridGuard

--- main.cpp
+++ main.cpp
@@ -1,54 +1,78 @@
 #include <iostream>
-#include "GridZone.h"
-#include "User.h"
-#include "FaultDetector.h"
-#include "ReportManager.h"
+#include <cstdlib>
+#include <ctime>
+#include <thread>
+#include <chrono>
 
 using namespace std;
 
-int main() {
+class Transformer {
+private:
+    float voltage;
+    float current;
+    float temperature;
 
-    User admin("admin", "1234");
+public:
 
-    string username;
-    string password;
-
-    cout << "===== GRIDGUARD LOGIN =====" << endl;
-
-    cout << "Username: ";
-    cin >> username;
-
-    cout << "Password: ";
-    cin >> password;
-     if(admin.login(username, password)) {
-
-        cout << "LOGIN SUCCESSFUL" << endl;
-
-        GridZone zone1("Zone A", 230, 12);
-        GridZone zone2("Zone B", 260, 18);
-
-        FaultDetector detector;
-        ReportManager report;
-
-        cout << endl;
-
-        zone1.displayStatus();
-        zone1.checkFault();
-
-        cout << endl;
-
-        zone2.displayStatus();
-        zone2.checkFault();
-
-        cout << endl;
-
-        detector.detectVoltage(260);
-        detector.detectCurrent(18);
-         report.saveReport("Zone B overload detected");
+    Transformer() {
+        voltage = 230;
+        current = 10;
+        temperature = 35;
     }
 
-    else {
-        cout << "INVALID LOGIN" << endl;
+    void generateReadings() {
+
+        voltage = 210 + rand() % 50;
+        current = 5 + rand() % 20;
+        temperature = 30 + rand() % 70;
+    }
+
+    void displayStatus() {
+
+        cout << "\n===== GRIDGUARD LIVE MONITOR =====\n";
+
+        cout << "Voltage: " << voltage << " V" << endl;
+        cout << "Current: " << current << " A" << endl;
+        cout << "Temperature: " << temperature << " C" << endl;
+
+        checkFaults();
+    }
+
+    void checkFaults() {
+
+        if(voltage > 240) {
+            cout << "ALERT: OVERVOLTAGE DETECTED\n";
+        }
+
+        if(current > 15) {
+            cout << "ALERT: OVERCURRENT DETECTED\n";
+        }
+
+        if(temperature > 80) {
+            cout << "ALERT: TRANSFORMER OVERHEATING\n";
+        }
+
+        if(current > 18 && voltage < 215) {
+            cout << "ALERT: POSSIBLE ILLEGAL CONNECTION DETECTED\n";
+        }
+    }
+};
+
+int main() {
+
+    srand(time(0));
+
+    Transformer transformer;
+
+    while(true) {
+
+        transformer.generateReadings();
+
+        transformer.displayStatus();
+
+        this_thread::sleep_for(chrono::seconds(2));
+
+        cout << "\n---------------------------------\n";
     }
 
     return 0;
